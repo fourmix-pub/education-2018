d2fcb4fd0fb658b47f33883eb855e5cefee7df3d
anagaho <anagaho@fourmix.co.jp>
2018-07-05 18:27:39 +0900

    kousin

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>掲示板</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js" charset="utf-8"></script>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card text-center">
+            <div class="card-header">
+              掲示板
+            </div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-md-12 text-right">
+                  <a href="#" class="btn btn-primary">お題作成</a>
+                </div>
+              </div>
+              <hr>
+              <div class="row">
+                <div class="col-md-12 text-left">
+                  <div class="alert alert-dark" role="alert">
+                <a href="#"> A simple dark alert—check it out!</a>
+                </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 text-left">
+                  <div class="alert alert-dark" role="alert">
+                <a href="#"> A simple dark alert—check it out!</a>
+                </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 text-left">
+                  <div class="alert alert-dark" role="alert">
+                <a href="#"> A simple dark alert—check it out!</a>
+                </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 text-left">
+                  <div class="alert alert-dark" role="alert">
+                <a href="#"> A simple dark alert—check it out!</a>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
