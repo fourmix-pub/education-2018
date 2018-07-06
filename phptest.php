0298c5c4ddc89aa2757401bac50aaa861ac3da09
anagaho <anagaho@fourmix.co.jp>
2018-07-06 10:41:54 +0900

    修正

--- phptest.php
+++ phptest.php
@@ -8,10 +8,11 @@
     <form action="receivetest.php" method=post>
       <p>名前<input type="text" name="namae"></p>
       <p>星座<input type="text" name="holoscope"></p>
-      <p>血液型<input type="radio" name="blood" value="A"></p>
-      <p>血液型<input type="radio" name="blood" value="B"></p>
-      <p>血液型<input type="radio" name="blood" value="O"></p>
-      <p>血液型<input type="radio" name="blood" value="AB"></p>
+      <BR>
+      <p>A型<input type="radio" name="blood" value="A"></p>
+      <p>B型<input type="radio" name="blood" value="B"></p>
+      <p>O型<input type="radio" name="blood" value="O"></p>
+      <p>AB型<input type="radio" name="blood" value="AB"></p>
 
       <input type = "submit" value ="送信"></p>
       </form>
