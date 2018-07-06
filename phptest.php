babbac099dbfb06ae54948d322379dcb49b42c6a
anagaho <anagaho@fourmix.co.jp>
2018-07-06 10:27:26 +0900

    test

---
+++ phptest.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="recevetest.php" method=post>
+      <p>名前<input type="text" name="namae"></p>
+      <imput type = "submit" value ="送信"></p>
+      </form>
+  </body>
+</html>
---
+++ receivetest.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>
+      <?php print($_POST ['namae']); ?>
+      さん、こんにちは。
+    </p>
+  </body>
+</html>
