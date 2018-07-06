a1bc64fd1b49db11a62d33d374eed3a6b4d9bf6c
anagaho <anagaho@fourmix.co.jp>
2018-07-06 11:44:54 +0900

    bootstrap適用

--- newcontents.php
+++ newcontents.php
@@ -1,4 +1,22 @@
-<div class="container">
-タイトル  <?php print($_POST ['title']); ?><br>
-コンテンツ  <?php print($_POST ['contents']); ?>
-</div>
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="container">
+    <div class="card">
+      <div class="card-header">
+        Featured
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">新規トピックス作成</h5>
+        <p class="card-text">タイトル  <?php print($_POST ['title']); ?><br>
+        コンテンツ  <?php print($_POST ['contents']); ?></p>
+        <input type="submit" value="送信" class="btn btn-primary">
+      </div>
+    </div>
+    </div>
+  </body>
+</html>
