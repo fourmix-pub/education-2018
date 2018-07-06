4e1e2b34139afc9759e3f761f65a5ba2ffad7940
anagaho <anagaho@fourmix.co.jp>
2018-07-06 11:26:07 +0900

    追加・更新

---
+++ newTopic.php
@@ -0,0 +1,7 @@
+
+<div class="container">
+  <form action="newcontents.php" method=post>
+  <p>タイトル<input type="text" name="title"></p>
+  <p>コンテンツ<input type="text" name="contents"></p>
+  </form>
+</div>
---
+++ newcontents.php
@@ -0,0 +1,4 @@
+<div class="container">
+タイトル  <?php print($_POST ['title']); ?>
+コンテンツ  <?php print($_POST ['contents']); ?>
+</div>
