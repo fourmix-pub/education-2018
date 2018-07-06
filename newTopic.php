9761f3f270599be8c2909651a0ba43caa9136f62
anagaho <anagaho@fourmix.co.jp>
2018-07-06 11:38:22 +0900

    更新

--- newTopic.php
+++ newTopic.php
@@ -3,5 +3,6 @@
   <form action="newcontents.php" method=post>
   <p>タイトル<input type="text" name="title"></p>
   <p>コンテンツ<input type="text" name="contents"></p>
+  <input type="submit" value="送信">
   </form>
 </div>
